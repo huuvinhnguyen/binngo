e75df60a0fcffe02676ee762142b3a6b66801956
Bin Nguyen <nguyen.huu.vinh@moneyforward.vn>
2023-06-16 14:28:46 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	// "web/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+import "github.com/huuvinhnguyen/binngo/web/controller"
+
+func main() {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	r.Static("/css", "./static/css")
+	r.Static("/img", "./static/img")
+	r.Static("/scss", "./static/scss")
+	r.Static("/vendor", "./static/vendor")
+	r.Static("/js", "./static/js")
+	r.StaticFile("/favicon.ico", "./img/favicon.ico")
+
+	r.LoadHTMLGlob("web/templates/**/*")
+	controller.Router(r)
+
+	log.Println("Server started")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
